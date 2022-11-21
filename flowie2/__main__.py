950a5d1ae7e2c7b884cec9542a19511225ea6fed
Roy-Kid <Roy-Kid@users.noreply.github.com>
2022-11-21 10:58:56 +0000

    ✅ Ready to clone and code.

--- flowie2/__main__.py
+++ flowie2/__main__.py
@@ -1,4 +1,4 @@
-"""Entry point for project_name."""
+"""Entry point for flowie2."""
 
 from .cli import main  # pragma: no cover
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for flowie2 package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("flowie2", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="flowie2",
+    version=read("flowie2", "VERSION"),
+    description="Awesome flowie2 created by Roy-Kid",
+    url="https://github.com/Roy-Kid/flowie2/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="Roy-Kid",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["flowie2 = flowie2.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
